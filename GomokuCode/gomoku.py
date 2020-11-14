f812f0dc7a159616f74eb4373bf578d9c6441df8
relhajj <relhajj@users.noreply.github.com>
2020-11-13 23:18:08 -0700

    finished defensive moves

--- GomokuCode/gomoku.py
+++ GomokuCode/gomoku.py
@@ -1,6 +1,7 @@
 # Gomoku Player 
 # CMPUT 355 Assignment 4 
 
+
 import numpy as np
 import pandas
 import random
@@ -8,48 +9,77 @@ from pattern_finder import GFG
 import time
 
 
+# INPUT:  row and column dimensions 
+# OUTPUT: returns 2D board matrix
 def create_board(row,col): 
-    return np.full((row, col), '.') 
+    board = np.full((row, col), '.')
+    return board 
 
 
+# INPUT:  2D board matrix and the x and y axis labels  
+# OUTPUT: prints boards
 def display(board, x_labels, y_labels):
     print(pandas.DataFrame(board, columns=x_labels, index=y_labels))
     
 
+# INPUT:  2D board matrix and the pattern_finder object  
+# OUTPUT: returns True if one of the players won
 def found_winner(gfg, board):
-    return gfg.patternSearch(board, 'xxxxx') or gfg.patternSearch(board, 'ooooo')
+    locations, x_winner = gfg.patternSearch(board, 'xxxxx')
+    locations, o_winner = gfg.patternSearch(board, 'ooooo')
+    return x_winner or o_winner
 
 
-# TODO: add error handling       
+# INPUT:  2D board matrix, current player, the x labels, and the pattern_finder object 
+# OUTPUT: the new state of the board and the next player 
+def get_user_next_move(x_labels, row_size):
+    
+    valid = False 
+    while not valid:
+        col = input("Choose a column: ").lower()
+        row = input("Choose a row: ")
+        if (col in x_labels) and (row.isdigit()) and (int(row) < row_size) and (int(row) >= 0):
+            valid = True
+        else:
+            print("You chose an invalid location, choose again.")
+        
+    return int(row), x_labels.index(col)
+
+
+# INPUT:  2D board matrix, current player, the x labels, and the pattern_finder object 
+# OUTPUT: the new state of the board and the next player 
 def alternate_moves(board, player, x_labels, gfg):
     
     # human is player 1 (x)
     if player == 1:
-        col_letter = input("Choose a column: ").lower()
-        col_number = x_labels.index(col_letter)
-        row_move = int(input("Choose a row: "))
-        board[row_move][col_number] = 'x'
+        row, col = get_user_next_move(x_labels, len(board[0]))
+        board[row][col] = 'x'
         next_player = 2
         
     # computer is player 2 (o)
     elif player == 2: 
-        move = computer_player_random(board, gfg)
-        #location = computer_player(board)
+        move = computer_player_random(board, gfg) 
+        #location = computer_player(board, gfg)
         board[move[0]][move[1]] = 'o'
         next_player = 1
     
     return board, next_player
 
+
+# INPUT:  2D board matrix and the pattern_finder object 
+# OUTPUT: returns the location of the next move 
 def computer_player_random(board, gfg):
     
     start_time = time.time()
     
     print("Computer is thinking... ")
+    
+    move = check_winning_move(board, gfg)
      
-    move = get_defensive_move(board, gfg)
+    if move == None: 
+        move = get_defensive_move(board, gfg)
     
     if move == None:
-    
         done = False
         while not done:
             move = random.randint(0, 14), random.randint(0, 14)
@@ -62,43 +92,65 @@ def computer_player_random(board, gfg):
     return move
 
 
-# INPUT:  2D board matrix
-# OUTPUT: returns a location for next move (tuple with the row, column)
+# INPUT:  2D board matrix and the pattern_finder object 
+# OUTPUT: returns the location of the next move 
 def computer_player(board, gfg):
     
-    move = check_winning_move(board)
+    start_time = time.time()
+    print("Computer is thinking... ")
+    distance_from_node = 5
+    
+    move = check_winning_move(board, gfg)
     
     if move == None:
             move = get_defensive_move(board, gfg)
     
     if move == None:
-        move = make_offensive_move(board)
+        possible_moves_dict = find_promising_cells(board, distance_from_node)
+        possible_moves_dict = assign_weights(board, possible_moves_dict)
+        move = best_move(board, dict_promising_cells)
+    
+    print("Computer chose row "+ str(move[0]) + " and column " + str(move[1]))
+    print("Computer took", str(time.time() - start_time), "to make a move")    
     
     return move
 
 
-# INPUT:  2D board matrix
-# OUTPUT: returns a location  (tuple with the row, column)
-def check_winning_move(board):
-    pass
+# INPUT:  2D board matrix, chain_locations
+# OUTPUT: returns the location of the next move
+def get_empty_cell(board, chain_locations):
+    for location in chain_locations:
+        if board[location[0], location[1]] == ".":
+            return location
+    return [-1,-1]
 
 
-# INPUT:  2D board matrix
-# OUTPUT: returns a location  (tuple with the row, column)
-def get_defensive_move(board, gfg):
-    
-    defensive_cells = ['.xxxx', 'x.xxx', 'xx.xx', 'xxx.x', 'xxxx.','.xxx.']
-
-    for chain in defensive_cells:
-        if gfg.patternSearch(board, chain): 
-            print("found defensive chain: " + chain)
+# INPUT:  2D board matrix, the pattern_finder object, and a list of chain locations to search through 
+# OUTPUT: returns the location of the empty cell in the chain if it exists
+def get_chain_location(gfg, board, chains):
+    for chain in chains:
+        chain_locations, found = gfg.patternSearch(board, chain)
+        if found: 
+            print("found chain: " + chain)  
+            loc = get_empty_cell(board, chain_locations)
+            print("Best next move is at " + str(loc))
             break 
-    
-    # TODO - need to change gfg so it returns the location and direction as well 
-    # if not found: loc = None 
-    
-    loc = None 
-    return loc
+    if not found: loc = None  
+    return loc 
+
+
+# INPUT:  2D board matrix and the pattern_finder object
+# OUTPUT: returns the location of the next move
+def check_winning_move(board, gfg):
+    winning_moves = ['.oooo', 'o.ooo', 'oo.oo','.ooo..']
+    return get_chain_location(gfg, board, winning_moves)
+
+
+# INPUT:  2D board matrix and the pattern_finder object 
+# OUTPUT: returns the location of the next best defensive move if it exists 
+def get_defensive_move(board, gfg):
+    defensive_cells = ['.xxxx', 'x.xxx', 'xx.xx', '.x.xx.', '.xxx..']
+    return get_chain_location(gfg, board, defensive_cells)
 
 
 # INPUT:  2D board matrix (list)
@@ -118,7 +170,7 @@ def assign_weights(board, dict_promising_cells):
     
 # INPUT:  2D board matrix
 #         dict_promising_cells (dictionary) - dict of {location:weight}
-# OUTPUT: returns the location of the best move (tuple with the row, column)
+# OUTPUT: returns the location of the best offensive move 
 def best_move(board, dict_promising_cells):
     return max(dict_promising_cells, key=dict_promising_cells.get)
 
@@ -140,12 +192,9 @@ def main():
     
     # play game 
     while game_continue:
-        
         print ("Player " + str(player) +"'s turn")
-        
         current_board_state, player = alternate_moves(current_board_state, player, x_labels, gfg)
         display(current_board_state, x_labels, y_labels)
-        
         if found_winner(gfg, current_board_state):
             print("Game Over")
             game_continue = False 
--- GomokuCode/pattern_finder.py
+++ GomokuCode/pattern_finder.py
@@ -1,45 +1,45 @@
-# Python3 program to search a word in a 2D grid 
+# This program searches for a given chain in a 2D grid 
 
-# We do not take credit for this code
+# We do not take credit for this code, but we have slightly changed it to fit our needs
 # This code is contributed by Yezheng Li 
 # Found this code on this website: https://www.geeksforgeeks.org/search-a-word-in-a-2d-grid-of-characters/
-
 
 class GFG: 
 	
 	def __init__(self): 
 		self.R = None
 		self.C = None
-		self.dir = [[-1, 0], [1, 0], [1, 1], 
-					[1, -1], [-1, -1], [-1, 1], 
-					[0, 1], [0, -1]] 
+		
+		self.dir = [[-1, 0],   # upwards vertically
+		            [1, 0],    # downwards vertically 
+		            [1, 1],    # downwards diagonally to the right
+		            [1, -1],   # downwards diagonally to the left
+		            [-1, -1],  # upwards diagonally to the left
+		            [-1, 1],   # upwards diagonally to the right
+		            [0, 1],    # left horizontally 
+		            [0, -1]]   # right horizontally 		
+		
 					
-	# This function searches in all 8-direction 
-	# from point(row, col) in grid[][] 
+	# This function searches in all 8-direction from point(row, col) in grid[][] 
 	def search2D(self, grid, row, col, word): 
 		
-		# If first character of word doesn't match 
-		# with the given starting point in grid. 
-		if grid[row][col] != word[0]: 
-			return False
+		direction = [0,0]
+		
+		# If first character of word doesn't match with the given starting point in grid. 
+		if grid[row][col] != word[0]: return 2, 2, False
 			
-		# Search word in all 8 directions 
-		# starting from (row, col) 
+		# Search word in all 8 directions starting from (row, col) 
 		for x, y in self.dir: 
 			
-			# Initialize starting point 
-			# for current direction 
+			# Initialize starting point for current direction 
 			rd, cd = row + x, col + y 
 			flag = True
 			
-			# First character is already checked, 
-			# match remaining characters 
+			# First character is already checked, match remaining characters 
 			for k in range(1, len(word)): 
 				
 				# If out of bound or not matched, break 
-				if (0 <= rd <self.R and
-					0 <= cd < self.C and
-					word[k] == grid[rd][cd]): 
+				if (0 <= rd <self.R and 0 <= cd < self.C and word[k] == grid[rd][cd]): 
 					
 					# Moving in particular direction 
 					rd += x 
@@ -48,25 +48,31 @@ class GFG:
 					flag = False
 					break
 			
-			# If all character matched, then 
-			# value of flag must be false		 
+			# If all character matched, then value of flag must be false		 
 			if flag: 
-				return True
-		return False
+				return x, y, True
+		return 2, 2, False
 		
-	# Searches given word in a given matrix 
-	# in all 8 directions	 
+		
+	# Searches given word in a given matrix in all 8 directions	 
 	def patternSearch(self, grid, word): 
 		
 		# Rows and columns in given grid 
 		self.R = len(grid) 
 		self.C = len(grid[0]) 
 		
-		# Consider every point as starting point 
-		# and search given word 
+		# Consider every point as starting point and search given word 
 		for row in range(self.R): 
 			for col in range(self.C): 
-				if self.search2D(grid, row, col, word): 
-					print("pattern found at " + str(row) + ', ' + str(col)) 				
-					return True 
-
+				x_dir, y_dir, found = self.search2D(grid, row, col, word)
+				if found: 
+					#print("pattern found at " + str(row) + ', ' + str(col)) 
+					chain_locations = [[row,col]]
+					for i in range(4):
+						row += x_dir
+						col += y_dir
+						chain_locations.append([row,col])
+					#print(str(chain_locations))	
+					#print("direction is " + str(x_dir) + ', ' + str(y_dir)) 
+					return chain_locations, True
+		return [], False 
