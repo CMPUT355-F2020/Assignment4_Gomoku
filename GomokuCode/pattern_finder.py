0bcf2fbf8ec8d6e6a62c8e0b92ab897ae71ee4b9
relhajj <relhajj@users.noreply.github.com>
2020-11-21 22:40:06 -0700

    pattern finder changes

--- GomokuCode/pattern_finder.py
+++ GomokuCode/pattern_finder.py
@@ -2,8 +2,9 @@
 
 # We do not take credit for this code, but we have adapted it to fit our needs
 # Found this code on this website: https://www.geeksforgeeks.org/search-a-word-in-a-2d-grid-of-characters/
+import numpy as np
 
-class GFG: 
+class Pattern: 
 	
 	def __init__(self): 
 		self.R = None
@@ -18,62 +19,77 @@ class GFG:
 		            [0, 1],    # left horizontally 
 		            [0, -1]]   # right horizontally 		
 		
-	# This function searches in all 8-direction from point(row, col) in grid[][] 
-	def search2D(self, grid, row, col, word): 
 		
+	# the search2D function searches all 8 directions for a chain in the board at location (row, col)  
+	def search2D(self, board, row, col, chain, full_search): 
+		
+		number_chains = 0
+		direction_x = 2
+		direction_y = 2
+		found = False 
 		direction = [0,0]
 		
-		# If first character of word doesn't match with the given starting point in grid. 
-		if grid[row][col] != word[0]: return 2, 2, False
+		# if first character doesn't match, stop
+		if board[row][col] != chain[0]: 
+			return 2, 2, False, 0
+			#return direction_x, direction_y, found, number_chains
 			
-		# Search word in all 8 directions starting from (row, col) 
+		# search all 8 directions 
 		for x, y in self.dir: 
-			
-			# Initialize starting point for current direction 
-			rd, cd = row + x, col + y 
+			 
+			row_next, col_next = row + x, col + y 
 			flag = True
 			
-			# First character is already checked, match remaining characters 
-			for k in range(1, len(word)): 
-				
-				# If out of bound or not matched, break 
-				if (0 <= rd <self.R and 0 <= cd < self.C and word[k] == grid[rd][cd]): 
-					
-					# Moving in particular direction 
-					rd += x 
-					cd += y 
+			# first character checked, match rest of chain 
+			for k in range(1, len(chain)): 
+				 
+				if (0 <= row_next < self.R and 0 <= col_next < self.C and chain[k] == board[row_next][col_next]): 
+					# move in particular direction 
+					row_next += x 
+					col_next += y 
 				else: 
+					# if out of bound or no match found, break
 					flag = False
 					break
-			
-			# If all character matched, then value of flag must be false		 
+					 
 			if flag: 
-				return x, y, True
-		return 2, 2, False
-		
-		
-	# Searches given word in a given matrix in all 8 directions	
+				if full_search == False:
+					return x, y, True, 1
+				
+				flag = False 
+				found = True 
+				direction_x = x
+				direction_y = y 
+				number_chains += 1
+				
+		return direction_x, direction_y, found, number_chains
 	
-	### Add a flag to know if it should return or check the entire board 
-	### Should return number of pattern matches found 0, 1 ... 
-	def patternSearch(self, grid, word): 
+	
+	# patternSearch function searches for the first instance of the chain in the entire board	
+	def patternSearch(self, board, chain, search_full_board): 
 		
-		# Rows and columns in given grid 
-		self.R = len(grid) 
-		self.C = len(grid[0]) 
+		self.R = len(board) 
+		self.C = len(board[0]) 
 		
-		# Consider every point as starting point and search given word 
-		for row in range(self.R): 
-			for col in range(self.C): 
-				x_dir, y_dir, found = self.search2D(grid, row, col, word)
-				if found: 
-					#print("pattern found at " + str(row) + ', ' + str(col)) 
-					chain_locations = [[row,col]]
-					for i in range(4):
-						row += x_dir
-						col += y_dir
-						chain_locations.append([row,col])
-					#print(str(chain_locations))	
-					#print("direction is " + str(x_dir) + ', ' + str(y_dir)) 
-					return chain_locations, True
-		return [], False 
+		# iterate through every cell in board to search for the chain 
+		if not search_full_board:
+			for row in range(self.R): 
+				for col in range(self.C): 
+					x_dir, y_dir, found, num_chains = self.search2D(board, row, col, chain, search_full_board)
+					if found: 
+						chain_locations = [[row,col]]
+						for i in range(4):
+							row += x_dir
+							col += y_dir
+							chain_locations.append([row,col])
+						return chain_locations, True, 0
+			return [], False, 0
+		
+		else:
+			counter = 0
+			for row in range(self.R): 
+				for col in range(self.C): 
+					x_dir, y_dir, found, num_chains = self.search2D(board, row, col, chain, search_full_board)
+					if found: counter += num_chains
+			return [], counter>0, counter//2 
+		
