c83352b618d4d0e4575e178b32144fcac1d77c39
lxian1 <44733170+lxian1@users.noreply.github.com>
2020-11-26 18:08:22 -0700

    final weights

--- Weights.py
+++ Weights.py
@@ -1,12 +1,12 @@
 class Weights:# To be trained
     def __init__(self):
         # for playing
-        self.w_1 = 22.72 # length 3 with 1 open end 
-        self.w_2 = 49.26 # length 3 with 2 open ends
-        self.w_3 = 15.72 # length 2 with 1 open end
-        self.w_4 = 26.64 # length 2 with 2 open ends---->attack
-        self.w_5 = 4.49  # length 1 with 1 open end
-        self.w_6 = 7.11  # length 1 with 2 open ends
+        self.w_1 = 23.72 # length 3 with 1 open end 
+        self.w_2 = 51.26 # length 3 with 2 open ends
+        self.w_3 = 16.12 # length 2 with 1 open end
+        self.w_4 = 27.64 # length 2 with 2 open ends---->attack
+        self.w_5 = 4.59  # length 1 with 1 open end
+        self.w_6 = 7.90  # length 1 with 2 open ends
 
         # for training
         self.fitness = 0 # evaluation
